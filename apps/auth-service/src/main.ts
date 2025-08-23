263ad2ff38eccdca9c903744c255d5583a4db2b9
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-24 05:52:40 +0600

    feat: add middleware in auth service

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,6 +1,8 @@
 import express from "express";
 
 import cors from "cors";
+import { errorMiddleware } from "../../../packages/error-handler/error-middleware";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -12,9 +14,14 @@ app.use(
   })
 );
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send({ message: "Hello API" });
 });
+
+app.use(errorMiddleware);
 
 const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
