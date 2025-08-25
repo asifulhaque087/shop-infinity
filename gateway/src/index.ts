c2f666380756687a8b2bdced702782839db51e40
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-25 19:33:45 +0600

    fix: fix the rate limiter in gateway

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import proxy from "express-http-proxy";
 import morgan from "morgan";
-import rateLimt from "express-rate-limit";
+// import rateLimit from "express-rate-limit";
+import { rateLimit, ipKeyGenerator } from "express-rate-limit"; // Don't forget to import it!
+
 import swaggerUi from "swagger-ui-express";
 import axios from "axios";
 import cookieParser from "cookie-parser";
@@ -25,13 +27,22 @@ app.use(cookieParser());
 app.set("trust proxy", 1);
 
 // ** --- rate limiting ---
-const limiter = rateLimt({
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: (req: any) => (req.user ? 1000 : 100),
+//   message: { error: "Too many request, please try again later!" },
+//   standardHeaders: true,
+//   legacyHeaders: true,
+//   keyGenerator: (req: any) => req.ip,
+// });
+
+const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: (req: any) => (req.user ? 1000 : 100),
   message: { error: "Too many request, please try again later!" },
   standardHeaders: true,
   legacyHeaders: true,
-  keyGenerator: (req: any) => req.ip,
+  keyGenerator: (req) => ipKeyGenerator(req.ip || ""),
 });
 
 app.use(limiter);
