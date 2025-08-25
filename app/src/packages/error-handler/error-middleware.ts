1b692d1cde8d78d9e1fd46105f880e9a2a720e48
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-25 20:16:27 +0600

    fix: add next in the error middleware

--- app/src/packages/error-handler/error-middleware.ts
+++ app/src/packages/error-handler/error-middleware.ts
@@ -1,7 +1,12 @@
 import { AppError } from "./index";
-import { Request, Response } from "express";
+import type { Request, Response, NextFunction } from "express";
 
-export const errorMiddleware = (err: Error, req: Request, res: Response) => {
+export const errorMiddleware = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (err instanceof AppError) {
     console.log(`Error ${req.method} ${req.url} - ${err.message}`);
 
