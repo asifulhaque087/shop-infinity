19dc88ecb54df3cf7e4050cc1e68001c20089494
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-25 23:37:08 +0600

    fix: load env in prims config

--- app/prisma.config.ts
+++ app/prisma.config.ts
@@ -1,5 +1,8 @@
 // prisma.config.ts
 import { defineConfig } from "@prisma/config";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
 
 export default defineConfig({
   schema: "./src/prisma/schema.prisma",
