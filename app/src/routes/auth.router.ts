4582cd3d1754da0184bb37158eca2d3e5bb83d64
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-25 20:16:59 +0600

    feat: add health route

--- app/src/routes/auth.router.ts
+++ app/src/routes/auth.router.ts
@@ -4,5 +4,10 @@ import { userRegistration } from "../controller/auth.controller";
 const router: Router = express.Router();
 
 router.post("/user-registration", userRegistration);
+router.get("/health", (req, res) => {
+  //   console.log(process.cwd());
+  //   return res.json(process.cwd())
+  return res.json({ message: "App is running" });
+});
 
 export default router;
