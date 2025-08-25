7849dfb0d054921caf2fcd565c5841616048ef45
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-25 18:34:55 +0600

    feat: add required files

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -3,6 +3,9 @@ import express from "express";
 import cors from "cors";
 import { errorMiddleware } from "../../../packages/error-handler/error-middleware";
 import cookieParser from "cookie-parser";
+import router from "./routes/auth.router";
+import SwaggerUi from "swagger-ui-express";
+const swaggerDocument = require("./swagger-output.json");
 
 const app = express();
 
@@ -21,12 +24,21 @@ app.get("/", (req, res) => {
   res.send({ message: "Hello API" });
 });
 
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerDocument));
+app.get("/docs-json", (req, res) => {
+  res.json(swaggerDocument);
+});
+
+// router
+app.get("/api", router);
+
 app.use(errorMiddleware);
 
 const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 const server = app.listen(port, () => {
   console.log(`Auth service is running at http://localhost:${port}/api`);
+  console.log(`Swagger Docs available at http://localhost:${port}/api`);
 });
 
 server.on("error", (err) => {
