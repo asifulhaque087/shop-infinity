394a2a20eb5c581c903628259dcf4e9dd3bf0d5a
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-25 19:14:56 +0600

    feat: add application backend

---
+++ app/src/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import SwaggerUi from "swagger-ui-express";
+import router from "@/routes/auth.router";
+import { errorMiddleware } from "@/packages/error-handler/error-middleware";
+const swaggerDocument = require("./swagger-output.json");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello API" });
+});
+
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerDocument));
+app.get("/docs-json", (req, res) => {
+  res.json(swaggerDocument);
+});
+
+// router
+app.get("/api", router);
+
+app.use(errorMiddleware);
+
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
+
+const server = app.listen(port, () => {
+  console.log(`Auth service is running at http://localhost:${port}/api`);
+  console.log(`Swagger Docs available at http://localhost:${port}/api`);
+});
+
+server.on("error", (err) => {
+  console.log("Server Error ", err);
+});
