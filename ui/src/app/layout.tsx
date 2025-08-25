aea6ec6529b8924192b25419e1dd7b07c018bd8a
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-26 01:46:41 +0600

    feat: add client service

---
+++ ui/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Poppins, Roboto } from "next/font/google";
+import "./globals.css";
+import Header from "@/shared/widgets";
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: "--font-roboto",
+});
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+export const metadata: Metadata = {
+  title: "Shop Infinity",
+  description: "Shop Infinity",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} ${poppins.variable}`}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ ui/src/app/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div className="">this is homepagepage</div>;
+};
+
+export default page;
