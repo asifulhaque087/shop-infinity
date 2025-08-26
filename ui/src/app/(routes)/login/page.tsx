2a14720d7cc45e0eeae3e84af7eca7dc508bbbf4
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-26 22:38:54 +0600

    feat: add login page

--- ui/src/app/(routes)/login/page.tsx
+++ ui/src/app/(routes)/login/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import GoogleButton from "@/shared/components/google-button";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
 import { Eye, EyeOff } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -25,7 +27,34 @@ const page = () => {
     formState: { errors },
   } = useForm<FormData>();
 
-  const onSubmit = (data: FormData) => {};
+  const loginMutation = useMutation({
+    mutationFn: async (data: FormData) => {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_SERVER_URI}/api/login-user`,
+        data,
+        {
+          withCredentials: true,
+        }
+      );
+      return response.data;
+    },
+
+    onSuccess: (data) => {
+      setServerError(null);
+      router.push("/");
+    },
+
+    onError: (error: AxiosError) => {
+      const errorMessage =
+        (error.response?.data as { message?: string })?.message ||
+        "Invalid credentials!";
+      setServerError(errorMessage);
+    },
+  });
+
+  const onSubmit = (data: FormData) => {
+    loginMutation.mutate(data);
+  };
 
   return (
     <div className="w-full py-10 min-h-[85vh] bg-[#f1f1f1]">
@@ -44,7 +73,7 @@ const page = () => {
 
           <p className="text-center text-gray-500 mb-4">
             Don't have an account?{" "}
-            <Link href={"signup"} className="text-blue-500">
+            <Link href={"/signup"} className="text-blue-500">
               Sign up
             </Link>
           </p>
@@ -121,10 +150,11 @@ const page = () => {
               </Link>
             </div>
             <button
+              disabled={loginMutation.isPending}
               type="submit"
               className="w-full text-lg cursor-pointer bg-black text-white py-2 rounded-lg"
             >
-              Login
+              {loginMutation.isPending ? "Login..." : "Login"}
             </button>
 
             {serverError && (
