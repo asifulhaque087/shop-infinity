3509dec5ff46d8d204370ce409f74f0dac57d072
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-26 23:40:58 +0600

    feat: update resetOtp method

--- ui/src/app/(routes)/signup/page.tsx
+++ ui/src/app/(routes)/signup/page.tsx
@@ -126,7 +126,11 @@ const page = () => {
     }
   };
 
-  const resendOtp = () => {};
+  const resendOtp = () => {
+    if (userData) {
+      signupMutation.mutate(userData);
+    }
+  };
 
   return (
     <div className="w-full py-10 min-h-[85vh] bg-[#f1f1f1]">
