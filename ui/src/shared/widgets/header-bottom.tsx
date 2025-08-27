91e9ee5b9f1417972432d2c86ee4644bcba4d60c
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-27 11:21:12 +0600

    feat: use dynamic user data in header bottom component

--- ui/src/shared/widgets/header-bottom.tsx
+++ ui/src/shared/widgets/header-bottom.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { navItems } from "@/configs/constants";
+import useUser from "@/hooks/useUser";
 import {
   AlignLeft,
   ChevronDown,
@@ -14,6 +15,9 @@ import { useEffect, useState } from "react";
 const HeaderBottom = () => {
   const [show, setShow] = useState(false);
   const [isSticky, setIsSticky] = useState(false);
+
+  const { user, isLoading } = useUser();
+  // console.log("user is ", user);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -79,18 +83,39 @@ const HeaderBottom = () => {
           {isSticky && (
             <div className="flex items-center gap-8">
               <div className="flex items-center gap-2">
-                <Link
-                  href={"/login"}
-                  className="border-2 w-[50px] h-[50px] flex items-center justify-center rounded-full border-[#010f1c1a]"
-                >
-                  {/* <ProfileIcon /> */}
-                  <User />
-                </Link>
+                {!isLoading && user ? (
+                  <>
+                    <Link
+                      href={"/profile"}
+                      className="border-2 w-[50px] h-[50px] flex items-center justify-center rounded-full border-[#010f1c1a]"
+                    >
+                      <User />
+                    </Link>
 
-                <Link href={"/login"}>
-                  <span className="block font-medium">Hello,</span>
-                  <span className="font-semibold">Sign In</span>
-                </Link>
+                    <Link href={"/profile"}>
+                      <span className="block font-medium">Hello,</span>
+                      <span className="font-semibold">
+                        {user?.name?.split(" ")[0]}
+                      </span>
+                    </Link>
+                  </>
+                ) : (
+                  <>
+                    <Link
+                      href={"/login"}
+                      className="border-2 w-[50px] h-[50px] flex items-center justify-center rounded-full border-[#010f1c1a]"
+                    >
+                      <User />
+                    </Link>
+
+                    <Link href={"/login"}>
+                      <span className="block font-medium">Hello,</span>
+                      <span className="font-semibold">
+                        {isLoading ? "..." : "Sign In"}
+                      </span>
+                    </Link>
+                  </>
+                )}
               </div>
               <div className="flex items-center gap-5">
                 <Link href={"/wishlist"} className="relative">
