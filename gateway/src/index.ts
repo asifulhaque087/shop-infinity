dd5c779b01338ded6036608860b8a9ef807def34
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-29 16:09:20 +0600

    feat: using internal network

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -51,7 +51,8 @@ app.get("/gateway-health", (req, res) => {
   res.send({ message: "Welcome to api-gateway!" });
 });
 
-app.use("/", proxy("http://localhost:6001"));
+// app.use("/", proxy("http://localhost:6001"));
+app.use("/", proxy("http://app:6001"));
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
