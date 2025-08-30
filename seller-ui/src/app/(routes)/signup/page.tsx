722243d68e633fe14bcee8db7e8e68e864702696
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-30 12:33:06 +0600

    feat: add step 2 & 3

--- seller-ui/src/app/(routes)/signup/page.tsx
+++ seller-ui/src/app/(routes)/signup/page.tsx
@@ -3,11 +3,12 @@
 import { useMutation } from "@tanstack/react-query";
 import { Eye, EyeOff } from "lucide-react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import { KeyboardEvent, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import axios, { AxiosError } from "axios";
 import { countries } from "@/utils/countries";
+import CreateShop from "@/shared/modules/create-shop";
 
 // type FormData = {
 //   name: string;
@@ -25,11 +26,11 @@ const page = () => {
   const [timer, setTimer] = useState(60);
   const [otp, setOtp] = useState(["", "", "", ""]);
   const [sellerData, setSellerData] = useState<FormData | null>(null);
-  const [sellerid, setSellerId] = useState("");
+  const [sellerId, setSellerId] = useState("");
 
   const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
 
-  const router = useRouter();
+  // const router = useRouter();
 
   const {
     register,
@@ -138,6 +139,20 @@ const page = () => {
     }
   };
 
+  const connectStipe = async () => {
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_SERVER_URI}/api/create-stripe-link`,
+        { sellerId }
+      );
+
+      if (response.data.url) {
+        window.location.href = response.data.url;
+      }
+    } catch (error) {
+      console.error("Stripe Connection Error:", error);
+    }
+  };
   return (
     <div className="w-full flex flex-col items-center pt-10 min-h-screen">
       {/* Stepper */}
@@ -372,6 +387,22 @@ const page = () => {
               </div>
             )}
           </>
+        )}
+
+        {activeStep === 2 && (
+          <CreateShop sellerId={sellerId} setActiveStep={setActiveStep} />
+        )}
+
+        {activeStep === 3 && (
+          <div className="text-center">
+            <h3 className="text-2xl font-semibold">Withdraw Method</h3>
+            <button
+              className="w-full m-auto flex items-center justify-center gap-3 text-lg  bg-[#334155] text-white py-2 rounded-lg mt-6 cursor-pointer"
+              onClick={connectStipe}
+            >
+              Connect Stripe
+            </button>
+          </div>
         )}
       </div>
     </div>
