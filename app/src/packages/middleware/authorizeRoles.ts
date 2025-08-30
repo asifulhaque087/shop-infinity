b400c1ed3d9a7b6c8ee53cb2f52d8e36fb64e8d6
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-30 12:34:43 +0600

    feat: add authorize roles middleware

---
+++ app/src/packages/middleware/authorizeRoles.ts
@@ -0,0 +1,16 @@
+import type { NextFunction, Response } from "express";
+import { AuthError } from "../error-handler";
+
+export const isSeller = (req: any, res: Response, next: NextFunction) => {
+  if (req.role !== "seller") {
+    throw new AuthError("Access denied: Seller only");
+  }
+  next();
+};
+
+export const isUser = (req: any, res: Response, next: NextFunction) => {
+  if (req.role !== "user") {
+    return next(new AuthError("Access denied: User only"));
+  }
+  next();
+};
