b2ef5717cc81d91ccac27ffbdd50c5207446fd8b
asifulhaque087 <asifulhaque087@gmail.com>
2025-08-30 23:47:46 +0600

    feat: update image place holder component

--- seller-ui/src/shared/components/image-placeholder/index.tsx
+++ seller-ui/src/shared/components/image-placeholder/index.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { Pencil, WandSparkles } from "lucide-react";
+import Image from "next/image";
 import { useState } from "react";
 
 // This interface defines the expected properties for a component.
@@ -48,6 +50,54 @@ const ImagePlaceHolder = (props: Props) => {
         id={`image-upload-${index}`}
         onChange={handleFileChange}
       />
+
+      {imagePreview ? (
+        <>
+          <button
+            type="button"
+            className="absolute top-3 p-2 rounded bg-blue-500 shadow-lg cursor-pointer"
+            onClick={() => setOpenImageModal(true)}
+          >
+            <WandSparkles size={16} />
+          </button>
+        </>
+      ) : (
+        <label
+          htmlFor={`image-upload-${index}`}
+          className="absolute top-3 right-3 p-2 rounded border-l-slate-700 shadow-lg cursor-pointer"
+        >
+          <Pencil size={16} />
+        </label>
+      )}
+
+      {imagePreview ? (
+        <Image
+          width={400}
+          height={300}
+          src={imagePreview}
+          alt="uploaded"
+          className="w-full  h-full object-cover rounded-lg"
+        />
+      ) : (
+        <>
+          <p
+            className={`text-gray-400 ${
+              small ? "text-xl" : "text-4xl"
+            } font-semibold`}
+          >
+            {size}
+          </p>
+
+          <p
+            className={`text-gray-500 ${
+              small ? "text-sm" : "text-lg"
+            } pt-2 text-center`}
+          >
+            Please choose an image <br />
+            according to the expected ration
+          </p>
+        </>
+      )}
     </div>
   );
 };
